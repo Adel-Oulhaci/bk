6436dd1dbc86d3865e8e0b3a77f31eb8a0475377
Adel Fakhr EL Islem OULHACI <35428516+Adel-Oulhaci@users.noreply.github.com>
2024-12-07 11:33:34 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN9Mitu2uUbYscNE0hNHXHDLODlEXdzYQ",
-  authDomain: "bkweb-630de.firebaseapp.com",
-  projectId: "bkweb-630de",
-  storageBucket: "bkweb-630de.firebasestorage.app",
-  messagingSenderId: "439322467395",
-  appId: "1:439322467395:web:c3b04eae424c525ab648f6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
